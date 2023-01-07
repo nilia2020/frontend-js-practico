808f18f003fcb863d96fb8b2d68b9a4ed9658679
jorge niglia <jorge.niglia@gmail.com>
2023-01-07 20:14:02 -0300

    hasta fusion del carrito

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const menuCartIcon = document.querySelector(".navbar-shopping-cart");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
 
-navEmail.addEventListener("click", toggleDescktopMenu);
+navEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCartIcon.addEventListener("click", toggleCarritoAside);
 
-function toggleDescktopMenu() {
+function toggleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.toggle("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
+}
+function toggleCarritoAside() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.toggle("inactive");
+  }
+  aside.classList.toggle("inactive");
 }
