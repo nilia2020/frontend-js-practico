3080d851a5afffb0e3942727c4eedb7b058c7941
jorge niglia <jorge.niglia@gmail.com>
2023-01-08 15:29:08 -0300

    hasta lista de productos

--- main.js
+++ main.js
@@ -27,3 +27,115 @@ function toggleCarritoAside() {
   }
   aside.classList.toggle("inactive");
 }
+
+const cardsContainer = document.querySelector(".cards-container");
+// product list
+const productList = [];
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Carpet",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276516/pexels-photo-276516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Dress",
+  price: 520,
+  image:
+    "https://images.pexels.com/photos/276518/pexels-photo-276518.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Carpet",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276516/pexels-photo-276516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Dress",
+  price: 520,
+  image:
+    "https://images.pexels.com/photos/276518/pexels-photo-276518.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Carpet",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276516/pexels-photo-276516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Dress",
+  price: 520,
+  image:
+    "https://images.pexels.com/photos/276518/pexels-photo-276518.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Bike",
+  price: 120,
+  image:
+    "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Carpet",
+  price: 220,
+  image:
+    "https://images.pexels.com/photos/276516/pexels-photo-276516.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+productList.push({
+  name: "Dress",
+  price: 520,
+  image:
+    "https://images.pexels.com/photos/276518/pexels-photo-276518.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+});
+function renderProductList(array) {
+  for (product of array) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+    const productImg = document.createElement("img");
+    productImg.setAttribute("src", product.image);
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+    const productPrice = document.createElement("p");
+    productPrice.innerText = `$${product.price}`;
+    const productName = document.createElement("p");
+    productName.innerText = `${product.name}`;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productFigure = document.createElement("figure");
+    const productCartIcon = document.createElement("img");
+    productCartIcon.setAttribute("src", "./icons/bt_add_to_cart.svg");
+
+    productFigure.appendChild(productCartIcon);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+  }
+}
+renderProductList(productList);
